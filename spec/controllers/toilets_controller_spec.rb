5b812d694809d396300394830670810d3d3c8071
Henrique Bastos <henrique@bastos.net>
2010-07-22 19:46:19 -0300

    Added 3 real toilets to database.

--- spec/controllers/toilets_controller_spec.rb
+++ spec/controllers/toilets_controller_spec.rb
@@ -9,7 +9,7 @@ describe ToiletsController do
     end
 
     it "should assign @toilets" do
-      assigns(:toilets).should ==  [{"latitude"=>-30.05914, "longitude"=>-51.175904}, {"latitude"=>30.059047, "longitude"=>-51.173013}, {"latitude"=>-30.06031, "longitude"=>-51.173646}, {"latitude"=>-30.055973, "longitude"=>-51.174898}]
+      assigns(:toilets).should ==  [{"latitude"=>-30.058381, "longitude"=>-51.175247}, {"latitude"=>-30.057963, "longitude"=>-51.174971}, {"latitude"=>-30.058497, "longitude"=>-51.17595}]
     end
     
   end
@@ -17,8 +17,8 @@ describe ToiletsController do
   describe "show" do
     
     it "should return toilet from lat_long" do
-      get :show, :id => "-30.059140,-51.175904"
-      assigns(:toilet).should == {"lat_long"=>"-30.059140,-51.175904", "description" => "Descrição do banheiro"}
+      get :show, :id => "-30.058381,-51.175247"
+      assigns(:toilet).should == {"working_hour"=>"todos os dias, das 8h às 21h.", "purpose"=>"nº 1 e nº 2", "public"=>"both", "title"=>"Prédio 8 - Faculdade de Letras", "tip"=>"Próximo à lanchonete do térreo.", "lat_long"=>"-30.058381,-51.175247", "capacity"=>"2 mictórios e 4 sanitários.", "address"=>"Avenida Ipiranga, 6681 - Prédio 8 - Porto Alegre - RS, 91530-000, Brasil - (0xx)51 3320-3500"}
     end
 
   end
--- spec/models/toilet_spec.rb
+++ spec/models/toilet_spec.rb
@@ -5,7 +5,7 @@ describe Toilet do
   describe ".all" do
 
     it "should return all toilets" do
-      Toilet.all.should ==  [{"lat_long"=>"-30.059140,-51.175904", "description"=>"Descrição do banheiro"}, {"lat_long"=>"30.059047,-51.173013", "description"=>"Descrição do banheiro2"}, {"lat_long"=>"-30.06031,-51.173646", "description"=>"Descrição do banheiro3"}, {"lat_long"=>"-30.055973,-51.174898"}]
+      Toilet.all.should ==  [{"working_hour"=>"todos os dias, das 8h às 21h.", "purpose"=>"nº 1 e nº 2", "public"=>"both", "title"=>"Prédio 8 - Faculdade de Letras", "tip"=>"Próximo à lanchonete do térreo.", "lat_long"=>"-30.058381,-51.175247", "capacity"=>"2 mictórios e 4 sanitários.", "address"=>"Avenida Ipiranga, 6681 - Prédio 8 - Porto Alegre - RS, 91530-000, Brasil - (0xx)51 3320-3500"}, {"working_hour"=>"todos os dias, das 8h às 21h.", "purpose"=>"nº 1 e nº 2", "public"=>"both", "title"=>"Prédio 7 - Famecos", "tip"=>"Vire a esquerda na lanchonete do térreo, suba as escadas, vire a esquerda. Fica em frente ao 212.", "lat_long"=>"-30.057963,-51.174971", "capacity"=>"4 sanitários.", "address"=>"Avenida Ipiranga, 6681 - Prédio 7 - Porto Alegre - RS, 91530-000, Brasil - (0xx)51 3320-3500"}, {"working_hour"=>"todos os dias, das 8h às 21h. (horário da PUC)", "purpose"=>"nº 1 e nº 2", "public"=>"both", "title"=>"Prédio 40 - 3º andar", "tip"=>"Suba o elevador até o 3º andar, vire a direita e siga pelo corredor.", "lat_long"=>"-30.058497,-51.17595", "capacity"=>"4 mictórios e 5 sanitários.", "address"=>"Avenida Ipiranga, 6681 - Prédio 40 - Porto Alegre - RS, 91530-000, Brasil - (0xx)51 3320-3500"}]
     end
 
   end
@@ -13,7 +13,7 @@ describe Toilet do
   describe ".all_to_json" do
 
     it "should return all toilets" do
-      Toilet.all_to_json.should == [{"latitude"=>-30.05914, "longitude"=>-51.175904}, {"latitude"=>30.059047, "longitude"=>-51.173013}, {"latitude"=>-30.06031, "longitude"=>-51.173646}, {"latitude"=>-30.055973, "longitude"=>-51.174898}]
+      Toilet.all_to_json.should == [{"latitude"=>-30.058381, "longitude"=>-51.175247}, {"latitude"=>-30.057963, "longitude"=>-51.174971}, {"latitude"=>-30.058497, "longitude"=>-51.17595}]
     end
 
   end
