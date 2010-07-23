7b414895a5232231b9a9a26dfc9a17e3f658c873
Vitor Pellegrino <vitorp@gmail.com>
2010-07-23 18:22:42 -0300

    Moving the geolocalization to the application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,4 +34,14 @@ function initialize() {
 
 $(document).ready(function(){
 	initialize();
+	$("#btnInit").click(initiate_geolocation)
 });
+
+function initiate_geolocation() {
+     navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+ }
+
+ function handle_geolocation_query(position){
+     alert('Lat: ' + position.coords.latitude + ' ' +
+           'Lon: ' + position.coords.latitude);
+ }
