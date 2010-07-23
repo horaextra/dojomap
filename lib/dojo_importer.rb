721a0d7600fc723061ec1a729c8cc61f2b576341
Vinicius Teles <vinicius@improveit.com.br>
2010-07-23 18:57:12 -0300

    Updating db

--- lib/dojo_importer.rb
+++ lib/dojo_importer.rb
@@ -1,11 +1,11 @@
 class DojoImporter
   
   def initialize
+    Dojo.destroy_all
     dojos = YAML.load(File.read("#{RAILS_ROOT}/db/dojos.yml"))
 
     dojos.each do |dojo|
-      d = Dojo.find_or_create_by_lat_long(dojo["lat_long"])
-      d.update_attributes(dojo)
+      Dojo.create(dojo)
     end
   end
   
